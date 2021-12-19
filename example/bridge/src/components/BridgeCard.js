85cb559ddf6199764d5e38a24e1403c19a891f05
Maoz Dror <maozdror@protonmail.com>
2021-12-19 00:43:40 -0500

    Update examples/bridge

--- example/bridge/src/components/BridgeCard.js
+++ example/bridge/src/components/BridgeCard.js
@@ -1,6 +1,8 @@
 import {
     Tokens,
-    Networks
+    Networks,
+    SwapPools,
+    swappableTokensAllNetworks,
 } from "@synapseprotocol/sdk";
 
 import {useEffect, useState} from "react";
@@ -9,12 +11,12 @@ import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 
 const ALL_NETWORKS = [
-    Networks.Arbitrum,
-    Networks.Avalanche,
+    Networks.ARBITRUM,
+    Networks.AVALANCHE,
     Networks.BSC,
-    Networks.Ethereum,
-    Networks.Fantom,
-    Networks.Polygon
+    Networks.ETH,
+    Networks.FANTOM,
+    Networks.POLYGON
 ]
 
 const NETWORK_OPTS = ALL_NETWORKS.map((net, idx) => {
@@ -25,6 +27,8 @@ function NetworkSelector({selectedNetwork, setSelectedNetwork, direction}) {
     let [selected, setSelected] = useState(
         NETWORK_OPTS.find((n) => n.value.name === selectedNetwork.name)
     )
+
+    console.log(swappableTokensAllNetworks());
 
     function onSelect(opt) {
         setSelected(opt);
@@ -51,7 +55,7 @@ function useTokensForNetwork(selectedNetwork) {
 
     useEffect(() => {
         if (selectedNetwork !== null) {
-            let tokens = Tokens.getAllSwappableTokensForNetwork(selectedNetwork.chainId);
+            let tokens = SwapPools.getAllSwappableTokensForNetwork(selectedNetwork.chainId);
             setTokensForNetwork(tokens)
         }
     }, [selectedNetwork])
@@ -165,7 +169,7 @@ function SelectorsForDirection({
 }
 
 function BridgeCard() {
-    let [networkFrom, setNetworkFrom] = useState(Networks.Ethereum)
+    let [networkFrom, setNetworkFrom] = useState(Networks.ETH)
     let [tokensForNetworkFrom] = useTokensForNetwork(networkFrom);
     let [tokenFrom, setTokenFrom] = useState(null)
     let [valueFrom, setValueFrom] = useState("0.0")
