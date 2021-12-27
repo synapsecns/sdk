3cb8e34c468f8d5cdbeb12f0579935239ef0ed53
Maoz Dror <88872576+maozdror@users.noreply.github.com>
2021-12-27 12:33:23 -0500

    Env rpc uris (#8)
    * add dotenv as a direct dependency
    
    * Implement loading RPC provider URLs from ENV

--- src/rpcproviders/index.ts
+++ src/rpcproviders/index.ts
@@ -2,19 +2,19 @@ import {JsonRpcProvider} from "@ethersproject/providers";
 
 import {ChainId} from "../common";
 
+import {rpcUriForChainId} from "./uris";
 
-const PROVIDERS: {[c:number]: JsonRpcProvider} = {
-    [ChainId.ETH]:       new JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v2/0AovFRYl9L7l4YUf6nPaMrs7H2_pj_Pf"),
-    [ChainId.OPTIMISM]:  new JsonRpcProvider("https://mainnet.optimism.io"),
-    [ChainId.BSC]:       new JsonRpcProvider("https://bsc-dataseed1.binance.org/"),
-    [ChainId.POLYGON]:   new JsonRpcProvider("https://polygon-rpc.com/"),
-    [ChainId.FANTOM]:    new JsonRpcProvider("https://rpc.ftm.tools/"),
-    [ChainId.BOBA]:      new JsonRpcProvider("https://replica-oolong.boba.network/"),
-    [ChainId.MOONRIVER]: new JsonRpcProvider("https://rpc.moonriver.moonbeam.network"),
-    [ChainId.ARBITRUM]:  new JsonRpcProvider("https://arb1.arbitrum.io/rpc"),
-    [ChainId.AVALANCHE]: new JsonRpcProvider("https://api.avax.network/ext/bc/C/rpc"),
-    [ChainId.HARMONY]:   new JsonRpcProvider("https://harmony-0-rpc.gateway.pokt.network/"),
-}
+interface RpcProviderMap {[c: number]: JsonRpcProvider}
+
+const PROVIDERS: RpcProviderMap = ((): RpcProviderMap => {
+    let m: RpcProviderMap = {};
+
+    ChainId.supportedChainIds().map((c) => {
+        m[c] = new JsonRpcProvider(rpcUriForChainId(c));
+    })
+
+    return m
+})()
 
 
 export const newProviderForNetwork = (chainId: number): JsonRpcProvider => PROVIDERS[chainId] ?? null
---
+++ src/rpcproviders/uris.ts
@@ -0,0 +1,57 @@
+import dotenv from 'dotenv'
+import {ChainId} from "../common";
+
+dotenv.config()
+
+const
+    ETH_RPC_URI_ENV: string = "ETH_RPC_URI",
+    OPTIMISM_RPC_URI_ENV: string = "OPTIMISM_RPC_URI",
+    BSC_RPC_URI_ENV: string = "BSC_RPC_URI",
+    POLYGON_RPC_URI_ENV: string = "POLYGON_RPC_URI",
+    FANTOM_RPC_URI_ENV: string = "FANTOM_RPC_URI",
+    BOBA_RPC_URI_ENV: string = "BOBA_RPC_URI",
+    MOONRIVER_RPC_URI_ENV: string = "MOONRIVER_RPC_URI",
+    ARBITRUM_RPC_URI_ENV: string = "ARBITRUM_RPC_URI",
+    AVALANCHE_RPC_URI_ENV: string = "AVALANCHE_RPC_URI",
+    HARMONY_RPC_URI_ENV: string = "HARMONY_RPC_URI";
+
+
+const ENV_KEY_MAP: {[c: number]: string} = {
+    [ChainId.ETH]:       ETH_RPC_URI_ENV,
+    [ChainId.OPTIMISM]:  OPTIMISM_RPC_URI_ENV,
+    [ChainId.BSC]:       BSC_RPC_URI_ENV,
+    [ChainId.POLYGON]:   POLYGON_RPC_URI_ENV,
+    [ChainId.FANTOM]:    FANTOM_RPC_URI_ENV,
+    [ChainId.BOBA]:      BOBA_RPC_URI_ENV,
+    [ChainId.MOONRIVER]: MOONRIVER_RPC_URI_ENV,
+    [ChainId.ARBITRUM]:  ARBITRUM_RPC_URI_ENV,
+    [ChainId.AVALANCHE]: AVALANCHE_RPC_URI_ENV,
+    [ChainId.HARMONY]:   HARMONY_RPC_URI_ENV,
+}
+
+const CHAIN_RPC_URIS: {[c: number]: string} = {
+    [ChainId.ETH]:       "https://eth-mainnet.alchemyapi.io/v2/0AovFRYl9L7l4YUf6nPaMrs7H2_pj_Pf",
+    [ChainId.OPTIMISM]:  "https://mainnet.optimism.io",
+    [ChainId.BSC]:       "https://bsc-dataseed1.binance.org/",
+    [ChainId.POLYGON]:   "https://polygon-rpc.com/",
+    [ChainId.FANTOM]:    "https://rpc.ftm.tools/",
+    [ChainId.BOBA]:      "https://replica-oolong.boba.network/",
+    [ChainId.MOONRIVER]: "https://rpc.moonriver.moonbeam.network",
+    [ChainId.ARBITRUM]:  "https://arb1.arbitrum.io/rpc",
+    [ChainId.AVALANCHE]: "https://api.avax.network/ext/bc/C/rpc",
+    [ChainId.HARMONY]:   "https://harmony-0-rpc.gateway.pokt.network/",
+}
+
+export const rpcUriForChainId = (chainId: number): string => {
+    return checkEnv(chainId) ?? CHAIN_RPC_URIS[chainId]
+}
+
+const checkEnv = (chainId: number): string|undefined => {
+    const envKey: string = ENV_KEY_MAP[chainId];
+
+    if (envKey in process.env) {
+        return process.env[envKey]
+    }
+
+    return undefined
+}
