0ea3a6edb95c51cc90855d1c24948f328a834fd0
Maoz Dror <maozdror@protonmail.com>
2022-01-02 19:30:34 -0500

    Add various test cases for Aurora

--- test/bridge/slippages-test.ts
+++ test/bridge/slippages-test.ts
@@ -2,23 +2,42 @@ import { BigNumber } from "@ethersproject/bignumber";
 import { expect } from "chai";
 
 import { Slippages } from "../../src";
+import {BigNumberish} from "ethers";
 
 describe("Slippages tests", function(this: Mocha.Suite) {
-    describe("_applySlippage", function(this: Mocha.Suite) {        
-        it("value of 69420 with 1% slippage should return 68725", () => {
-            const ret = Slippages._applySlippage(BigNumber.from(69420), "foo");
-            // This should default to 1% slippage -> (value - value * 0.01).
-            expect(ret._hex).to.equal(BigNumber.from(68725)._hex);
-        })
+    interface TestCase {
+        func:    (BigNumber, string) => BigNumber,
+        value:    BigNumber,
+        slippage: string,
+        expected: BigNumber,
+    }
 
-        it("value of 1337 with TWO_TENTH slippage should return 1334", () => {
-            const ret = Slippages._applySlippage(BigNumber.from(1337), "TWO_TENTH");
-            expect(ret._hex).to.equal(BigNumber.from(1334)._hex);
-        })
+    describe("_applySlippage", function(this: Mocha.Suite) {
+        const makeTitle = (tc: TestCase): string => `value of ${tc.value.toString()} with slippage of ${tc.slippage} should return ${tc.expected.toString()}`
 
-        it("value of 101 with QUARTER slippage should return 98", () => {
-            const ret = Slippages._applySlippage(BigNumber.from(101), Slippages.Quarter);
-            expect(ret._hex).to.equal(BigNumber.from(98)._hex);
+        let testCases: TestCase[] = [
+            {
+                func:     Slippages._applySlippage,
+                value:    BigNumber.from(69420),
+                slippage: "foo", // This should default to 1% slippage -> (value - value * 0.01).
+                expected: BigNumber.from(68725),
+            },
+            {
+                func:     Slippages._applySlippage,
+                value:    BigNumber.from(1337),
+                slippage: "TWO_TENTH",
+                expected: BigNumber.from(1334),
+            },
+            {
+                func:     Slippages._applySlippage,
+                value:    BigNumber.from(101),
+                slippage: Slippages.Quarter,
+                expected: BigNumber.from(98),
+            }
+        ]
+
+        testCases.forEach((tc: TestCase) => {
+            it(makeTitle(tc), () => expect(tc.func(tc.value, tc.slippage)._hex).to.equal(tc.expected._hex))
         })
     })
 
@@ -34,24 +53,24 @@ describe("Slippages tests", function(this: Mocha.Suite) {
     })
 
     describe("formatSlippageToString", function(this: Mocha.Suite) {
-        it("Slippages.One should return 1.0", () => {
-            expect(Slippages.formatSlippageToString(Slippages.One)).to.equal("1.0");
-        })
+        it("Slippages.One should return 1.0", () =>
+            expect(Slippages.formatSlippageToString(Slippages.One)).to.equal("1.0")
+        )
         
-        it("Slippages.OneTenth should return 0.1", () => {
-            expect(Slippages.formatSlippageToString(Slippages.OneTenth)).to.equal("0.1");
-        })
+        it("Slippages.OneTenth should return 0.1", () =>
+            expect(Slippages.formatSlippageToString(Slippages.OneTenth)).to.equal("0.1")
+        )
 
-        it("Slippages.TwoTenth should return 0.2", () => {
-            expect(Slippages.formatSlippageToString(Slippages.TwoTenth)).to.equal("0.2");
-        })
+        it("Slippages.TwoTenth should return 0.2", () =>
+            expect(Slippages.formatSlippageToString(Slippages.TwoTenth)).to.equal("0.2")
+        )
 
-        it("Slippages.Quarter should return 2.0", () => {
-            expect(Slippages.formatSlippageToString(Slippages.Quarter)).to.equal("2.0");
-        })
+        it("Slippages.Quarter should return 2.0", () =>
+            expect(Slippages.formatSlippageToString(Slippages.Quarter)).to.equal("2.0")
+        )
 
-        it("foo should return N/A", () => {
-            expect(Slippages.formatSlippageToString("foo")).to.equal("N/A");
-        })
+        it("foo should return N/A", () =>
+            expect(Slippages.formatSlippageToString("foo")).to.equal("N/A")
+        )
     })
 })
