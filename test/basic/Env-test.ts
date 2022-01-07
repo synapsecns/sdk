f73244857ad074e8c1b1198c4cd956b246e70935
Maoz Dror <maozdror@protonmail.com>
2022-01-07 09:50:58 -0500

    Fix Env-test so that it doesn't futz the whole thing

--- test/basic/Env-test.ts
+++ test/basic/Env-test.ts
@@ -1,10 +1,11 @@
 import path from "path";
 
 import {expect} from "chai";
-import {before, Done} from "mocha";
+import {before, Done, test} from "mocha";
 
 import dotenv from "dotenv";
 import {ChainId} from "../../src";
+import * as fs from "fs";
 
 const TEST_ENV_PATH: string = path.join("./", "test", "basic", "test_env.env");
 
@@ -13,14 +14,27 @@ describe('Test ENV values', function(this: Mocha.Suite) {
         describe("Test setting URIs from ENV", function(this: Mocha.Suite) {
             const valPrefix: string = "carl_";
 
-            dotenv.config({path: TEST_ENV_PATH});
-
             type testCase = [number, string, string];
             const testCases: testCase[] = [
                 [ChainId.AVALANCHE, "AVALANCHE", "AVALANCHE_RPC_URI"],
                 [ChainId.MOONRIVER, "MOONRIVER", "MOONRIVER_RPC_URI"],
-                [ChainId.ETH, "ETH", "ETH_RPC_URI"],
+                [ChainId.ETH,       "ETH",       "ETH_RPC_URI"],
             ];
+
+            let envBackup: {[s: string]: string} = {}
+
+            before("backup env", function(this: Mocha.Context) {
+                testCases.forEach((tc: testCase) => envBackup[tc[2]] = process.env[tc[2]])
+
+                let testEnvConfig = dotenv.parse(fs.readFileSync(TEST_ENV_PATH));
+                Object.keys(testEnvConfig).forEach((k: string) => {
+                    process.env[k] = testEnvConfig[k];
+                })
+            })
+
+            after(function(this: Mocha.Context) {
+                testCases.forEach((tc: testCase) => process.env[tc[2]] = envBackup[tc[2]])
+            })
 
             testCases.forEach((tc) => {
                 let [chainId, name, envKey] = tc;
