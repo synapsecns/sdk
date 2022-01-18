2ac4113842cf6b398aac95f7a0315f85246a901c
Maoz Dror <maozdror@protonmail.com>
2022-01-18 18:25:15 -0500

    More ERC20 tests for coverage

--- test/erc20/ERC20-test.ts
+++ test/erc20/ERC20-test.ts
@@ -20,29 +20,42 @@ import {BigNumber} from "ethers";
 describe("ERC20 tests", function(this: Mocha.Suite) {
     const testAddr: string = "0xe972647539816442e0987817DF777a9fd9878650";
 
-    const tokenParams: ERC20.ERC20TokenParams = {
-        chainId:      ChainId.BSC,
-        tokenAddress: Tokens.NUSD.address(ChainId.BSC),
-    }
+    const tokenParams = (c: number): ERC20.ERC20TokenParams => ({
+        chainId:      c,
+        tokenAddress: Tokens.NUSD.address(c),
+    })
 
     describe("Build approve transaction", function(this: Mocha.Suite) {
-        const args: ERC20.ApproveArgs = {
-            spender: SynapseContracts.BSC.bridge_zap.address,
+        interface TestCase {
+            chainId: number,
+            address: string
         }
 
-        it("should build a transaction successfully", function(this: Context, done: Done) {
-            let prom: Promise<PopulatedTransaction> = ERC20.buildApproveTransaction(args, tokenParams);
+        let testCases: TestCase[] = [
+            { chainId: ChainId.BSC,       address: SynapseContracts.BSC.bridge_zap.address },
+            { chainId: ChainId.ETH,       address: SynapseContracts.Ethereum.bridge_zap.address },
+            { chainId: ChainId.AVALANCHE, address: SynapseContracts.Avalanche.bridge_zap.address },
+        ]
 
-            expect(prom).to
-                .eventually.be.fulfilled
-                .and.to.eventually.not.be.null
-                .notify(done);
+        testCases.forEach(({chainId, address}) => {
+            const args: ERC20.ApproveArgs = {
+                spender: address,
+            }
+
+            it("should build a transaction successfully", function(this: Context, done: Done) {
+                let prom: Promise<PopulatedTransaction> = ERC20.buildApproveTransaction(args, tokenParams(chainId));
+
+                expect(prom).to
+                    .eventually.be.fulfilled
+                    .and.to.eventually.not.be.null
+                    .notify(done);
+            })
         })
     })
 
     describe("Balance of test", function(this: Mocha.Suite) {
         it("should have an nUSD balance greater than zero", function(this: Context, done: Done) {
-            let prom: Promise<BigNumber> = ERC20.balanceOf(testAddr, tokenParams)
+            let prom: Promise<BigNumber> = ERC20.balanceOf(testAddr, tokenParams(ChainId.BSC))
 
             expect(prom).to.eventually.be.gt(0).notify(done);
         })
@@ -50,7 +63,7 @@ describe("ERC20 tests", function(this: Mocha.Suite) {
 
     describe("allowanceOf test", function(this: Mocha.Suite) {
         it("synapsebridgezap should have an nUSD allowance gte zero", function(this: Context, done: Done) {
-            let prom: Promise<BigNumber> = ERC20.balanceOf(testAddr, tokenParams)
+            let prom: Promise<BigNumber> = ERC20.balanceOf(testAddr, tokenParams(ChainId.BSC))
 
             expect(prom).to.eventually.be.gte(0).notify(done);
         })
