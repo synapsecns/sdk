d6c988327471139c56da34d0655e8453354fb4d4
Maoz Dror <maozdror@protonmail.com>
2022-01-23 09:39:22 -0500

    wrapper func

--- src/bridge/gasutils.ts
+++ src/bridge/gasutils.ts
@@ -10,29 +10,31 @@ export namespace GasUtils {
         approveGasLimit?: BigNumber,
     }
 
+    const makeGwei = (n: string): BigNumber => parseUnits(n, "gwei")
+
     const ETH_GAS_PARAMS: GasParams = {
-        maxPriorityFee: parseUnits("1.5", "gwei"),
+        maxPriorityFee: makeGwei("1.5"),
         bridgeGasLimit: BigNumber.from(100000)
     };
 
     const BOBA_GAS_PARAMS: GasParams = {
-        gasPrice:        parseUnits("10", "gwei"),
+        gasPrice:        makeGwei("10"),
         approveGasLimit: BigNumber.from(60000),
     };
 
     const ARBITRUM_GAS_PARAMS: GasParams = {
-        gasPrice:       parseUnits("2.5", "gwei"),
+        gasPrice:       makeGwei("2.5"),
         bridgeGasLimit: BigNumber.from(1500000),
     };
 
     const AVALANCHE_GAS_PARAMS: GasParams = {
-        gasPrice:        parseUnits("150", "gwei"),
+        gasPrice:        makeGwei("150"),
         bridgeGasLimit:  BigNumber.from(800000),
         approveGasLimit: BigNumber.from(75000),
     }
 
     const AURORA_GAS_PARAMS: GasParams = {
-        gasPrice: parseUnits('0', 'gwei'),
+        gasPrice: makeGwei('0'),
     }
 
     export function makeGasParams(chainId: number): GasParams {
