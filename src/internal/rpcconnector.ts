dee687df1b0f2249033495322df887ca17ce0926
Bleaker <maozdror@protonmail.com>
2022-02-24 15:53:27 -0500

    internal/rpcconnector: remove unused interface methods

--- src/internal/rpcconnector.ts
+++ src/internal/rpcconnector.ts
@@ -7,8 +7,6 @@ import {MiniRpcProvider} from "./minirpc";
 
 export interface ProviderConnector<T extends Provider> {
     provider:          (chainId: number) => T;
-    providerWithUri:   (chainId: number, uri: string) => T;
-    setProviderUri:    (chainId: number, uri: string) => void;
     supportedChainIds: () => number[];
 }
 
@@ -18,7 +16,7 @@ class AbstractProviderConnector<T extends Provider> implements ProviderConnector
     protected newProviderFn:  (uri: string) => T;
 
     constructor(args: {
-        urls: StringMap,
+        urls:        StringMap,
         newProvider: (uri: string) => T
     }) {
         const {urls, newProvider} = args;
@@ -35,58 +33,33 @@ class AbstractProviderConnector<T extends Provider> implements ProviderConnector
         return this.providers[chainId]
     }
 
-    providerWithUri(chainId: number, uri: string): T {
-        return this.newProviderFn(uri)
-    }
-
-    /**
-     * Re-initializes the Provider instance for a given chain ID using the passed URI.
-     * This operation is permanent for the lifetime of whatever is using the RpcConnector.
-     * @param chainId
-     * @param uri
-     */
-    setProviderUri(chainId: number, uri: string) {
-        if (chainId in this.providers) {
-            delete this.providers[chainId];
-        }
-
-        this.providers[chainId] = this.newProviderFn(uri);
-    }
-
     supportedChainIds(): number[] {
         return Object.keys(this.urls).map(cid => Number(cid))
     }
 }
 
-export class JsonRpcConnector extends AbstractProviderConnector<JsonRpcProvider> implements ProviderConnector<JsonRpcProvider> {
-    constructor(args: {urls: StringMap}) {
+interface RpcConnectorArgs {
+    urls:             StringMap,
+    batchWaitTimeMs?: number,
+}
+
+export class JsonRpcConnector
+    extends AbstractProviderConnector<JsonRpcProvider>
+    implements ProviderConnector<JsonRpcProvider>
+{
+    constructor(args: RpcConnectorArgs) {
         super({
             ...args,
             newProvider: (uri: string) => new JsonRpcProvider(uri)
         });
     }
-
-    /**
-     * Returns a potentially new {@link JsonRpcProvider} instance for the given chain ID
-     * using the passed uri for the connection.
-     * If the passed uri is the same as the uri for a pre-existing JsonRpcProvider instance,
-     * said instance is returned rather than instantiating a brand new one.
-     * @param chainId
-     * @param uri
-     * @override
-     */
-    providerWithUri(chainId: number, uri: string): JsonRpcProvider {
-        const _existing = this.providers[chainId];
-        if (_existing?.connection.url === uri) {
-            return _existing
-        }
-
-        return super.providerWithUri(chainId, uri)
-    }
 }
 
-export class Web3RpcConnector extends AbstractProviderConnector<Web3Provider> implements ProviderConnector<Web3Provider> {
-    constructor(args: {urls: StringMap, batchWaitTimeMs?: number}) {
+export class Web3RpcConnector
+    extends AbstractProviderConnector<Web3Provider>
+    implements ProviderConnector<Web3Provider>
+{
+    constructor(args: RpcConnectorArgs) {
         let invertedUrlsMap = Object.keys(args.urls).reduce((acc, chainId) => {
             const cid = Number(chainId);
             const url = args.urls[cid];
@@ -101,23 +74,5 @@ export class Web3RpcConnector extends AbstractProviderConnector<Web3Provider> im
                 return new Web3Provider(provider)
             }
         });
-    }
-
-    /**
-     * Returns a potentially new {@link Web3Provider} instance for the given chain ID
-     * using the passed uri for the connection.
-     * If the passed uri is the same as the uri for a pre-existing JsonRpcProvider instance,
-     * said instance is returned rather than instantiating a brand new one.
-     * @param chainId
-     * @param uri
-     * @override
-     */
-    providerWithUri(chainId: number, uri: string): Web3Provider {
-        const _existing = this.providers[chainId];
-        if (_existing?.connection.url === uri) {
-            return _existing
-        }
-
-        return super.providerWithUri(chainId, uri)
     }
 }
