90bd677bbe9658424854eff57f83f08faf8c3c5b
Bleaker <maozdror@protonmail.com>
2022-03-14 16:27:49 -0400

    examples/frontend: provide function showing how to deal with tokens ...
    ... which have a .decimals() value which isn't 18.

--- examples/frontend/src/pages/bridge/components/BridgeEstimateSection.tsx
+++ examples/frontend/src/pages/bridge/components/BridgeEstimateSection.tsx
@@ -11,6 +11,7 @@ import {SetStateFunction, valueEther} from "@utils";
 import DarkRoundedItem from "@components/DarkRoundedItem";
 import {NetworkMenuContext} from "@pages/bridge/contexts/NetworkMenuContext";
 import {Networks} from "@synapseprotocol/sdk";
+import {parseUnits} from "@ethersproject/units";
 
 interface BridgeEstimateSectionProps {
     amountIn:   BigNumber,
@@ -24,14 +25,35 @@ const LabeledItem = ({title, value}) => (
     </div>
 )
 
-function formatValue(t: Token, networkTo: Networks.Network | number, value: string): string {
+function fixWeiValue(wei: BigNumber, tokenDecimals: number): BigNumber {
+    console.log("=== fixWeiValue ===");
+    console.log({ wei: wei.toString(), tokenDecimals });
+    const mul = BigNumber.from(10).pow(18 - tokenDecimals);
+    console.log({ mul: mul.toString() });
+    const ret = wei.mul(mul);
+    console.log({ ret: ret.toString() });
+
+    return ret
+}
+
+function formatValue(t: Token, networkTo: Networks.Network | number, value: string, fixWei: boolean = false): string {
     const chainIdTo = networkTo instanceof Networks.Network
         ? networkTo.chainId
         : networkTo;
 
-    const tokenDecimals: number = t.decimals(chainIdTo);
+    const
+        tokenDecimals: number = t.decimals(chainIdTo),
+        valueBn = BigNumber.from(value);
 
-    const ether: string = valueEther(value, {round: true, tokenDecimals});
+    const weiValue: BigNumber = fixWei
+        ? fixWeiValue(valueBn, tokenDecimals)
+        : valueBn;
+
+    const roundingPlaces = fixWei
+        ? (tokenDecimals <= 6 ? 3 : 6)
+        : 6;
+
+    const ether: string = valueEther(weiValue, {round: true, places: roundingPlaces});
     return `${ether} ${t.symbol}`
 }
 
@@ -50,7 +72,7 @@ export default function BridgeEstimateSection(props: BridgeEstimateSectionProps)
             {estimate &&
                 <LabeledItem
                     title={"Estimated output"}
-                    value={formatValue(tokenTo, selectedNetworkTo, estimate.toString())}
+                    value={formatValue(tokenTo, selectedNetworkTo, estimate.toString(), true)}
                 />
             }
             {fee &&
