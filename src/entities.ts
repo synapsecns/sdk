3bbc4403e9470af941e4d7390a3dd3bdb64cb208
Bleaker <maozdror@protonmail.com>
2022-03-30 02:46:46 -0400

    entities: add instance maker for AvaxJewelMigration contract

--- src/entities.ts
+++ src/entities.ts
@@ -3,11 +3,13 @@ import {
     L1BridgeZapFactory,
     L2BridgeZapFactory,
     BridgeConfigV3Factory,
+    AvaxJewelMigrationFactory,
     type SynapseBridgeContract,
     type L1BridgeZapContract,
     type L2BridgeZapContract,
     type GenericZapBridgeContract,
     type BridgeConfigV3Contract,
+    type AvaxJewelMigrationContract
 } from "./contracts";
 
 import {ChainId}             from "@chainid";
@@ -19,6 +21,8 @@ import type {SignerOrProvider} from "@common/types";
 
 
 const bridgeConfigV3Address: string = "0x5217c83ca75559B1f8a8803824E5b7ac233A12a1";
+
+export const AvaxJewelMigrationAddress: string = "0x82d4aCF0DA013Ee3649C7eAdF5Db9093A7EFa7B0";
 
 enum ContractKind {bridge="bridge", bridgeZap="bridgeZap"}
 
@@ -58,5 +62,12 @@ export function BridgeConfigV3ContractInstance(): BridgeConfigV3Contract {
     return BridgeConfigV3Factory.connect(
         bridgeConfigV3Address,
         rpcProviderForChain(ChainId.ETH)
+    )
+}
+
+export function AvaxJewelMigrationContractInstance(): AvaxJewelMigrationContract {
+    return AvaxJewelMigrationFactory.connect(
+        AvaxJewelMigrationAddress,
+        rpcProviderForChain(ChainId.AVALANCHE)
     )
 }
