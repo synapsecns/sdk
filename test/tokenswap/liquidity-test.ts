1bdd6ccfcb550ce3f745ad38faa999b2676e5d4c
Bleaker <maozdror@protonmail.com>
2022-04-19 04:57:07 -0400

    tests/tokenswap: add tests for calculateRemoveLiquidityOneToken

--- test/tokenswap/liquidity-test.ts
+++ test/tokenswap/liquidity-test.ts
@@ -63,9 +63,9 @@ describe("Liquidity tests", function(this: Mocha.Suite) {
 				this.slow(2 * 1000);
 
 				const gotProm: Promise<BigNumber> = TokenSwap.calculateAddLiquidity({
-					chainId: 		tc.chainId,
-					lpSwapAddress:  tc.lpToken.swapAddress,
-					amounts:		makeAmountsArray(tc.liquidityToken, tc.inputAmount, tc.lpToken)
+					chainId:  tc.chainId,
+					lpToken:  tc.lpToken,
+					amounts:  makeAmountsArray(tc.liquidityToken, tc.inputAmount, tc.lpToken)
 				});
 
 				let got: BigNumber;
@@ -135,9 +135,9 @@ describe("Liquidity tests", function(this: Mocha.Suite) {
 				this.slow(3.5 * 1000);
 
 				const gotProm: Promise<BigNumber[]> = TokenSwap.calculateRemoveLiquidity({
-					chainId: 		tc.chainId,
-					lpSwapAddress:  tc.lpToken.swapAddress,
-					amount:			tc.withdrawAmount
+					chainId:  tc.chainId,
+					lpToken:  tc.lpToken,
+					amount:   tc.withdrawAmount
 				});
 
 				let got: BigNumber[];
@@ -157,6 +157,82 @@ describe("Liquidity tests", function(this: Mocha.Suite) {
 				}
 
 				return Promise.all(got.map(val => expect(val).to.be.gt(Zero)))
+			});
+		});
+	});
+
+	describe("calculateRemoveLiquidityOneToken tests", function(this: Mocha.Suite) {
+		interface TestCase {
+			chainId:         ChainId;
+			lpToken:         SwapPools.SwapPoolToken;
+			poolToken:		 Token;
+			withdrawAmount:  BigNumber;
+			wantZero:		 boolean;
+			wantError:		 boolean;
+		}
+
+		function makeTestCase(
+			chainId: 		 ChainId,
+			lpToken: 		 SwapPools.SwapPoolToken,
+			poolToken:		 Token,
+			wantZero:        boolean,
+			wantError:       boolean = false,
+			withdrawAmount?: string | BigNumber
+		): TestCase {
+			let amt: BigNumber;
+			if (withdrawAmount && withdrawAmount instanceof BigNumber) {
+				amt = withdrawAmount as BigNumber;
+			} else {
+				amt = getTestAmount(lpToken.baseToken, chainId, withdrawAmount);
+			}
+
+			return {chainId, lpToken, poolToken, withdrawAmount: amt, wantError, wantZero}
+		}
+
+		const testCases: TestCase[] = [
+			makeTestCase(ChainId.BSC,       SwapPools.BSC_POOL_SWAP_TOKEN,       Tokens.BUSD,       false, false, "55"),
+			makeTestCase(ChainId.AVALANCHE, SwapPools.AVALANCHE_POOL_SWAP_TOKEN, Tokens.DAI,        false, false, "550"),
+			makeTestCase(ChainId.CRONOS,    SwapPools.CRONOS_POOL_SWAP_TOKEN,    Tokens.NUSD,       false, false, "15"),
+			makeTestCase(ChainId.CRONOS,    SwapPools.CRONOS_POOL_SWAP_TOKEN,    Tokens.NUSD,       true,  false, "0"),
+			makeTestCase(ChainId.CRONOS,    SwapPools.CRONOS_POOL_SWAP_TOKEN,    Tokens.NUSD,       false, false, BigNumber.from("50000000000000000")),
+			makeTestCase(ChainId.CRONOS,    SwapPools.CRONOS_POOL_SWAP_TOKEN,    Tokens.NUSD,       true,  true,  BigNumber.from("50000000000000000000000000")),
+			makeTestCase(ChainId.METIS,     SwapPools.METIS_ETH_SWAP_TOKEN,      Tokens.METIS_ETH,  false, false, "550"),
+		];
+
+		testCases.forEach(tc => {
+			const
+				titleParams: string = `Chain ID: ${tc.chainId}, amount: ${formatEther(tc.withdrawAmount)}`,
+				titleSuffix: string = tc.wantError ? "should error out" : `should${tc.wantZero ? "" : " not"} return zero`,
+				wantTitle:   string = `calculateRemoveLiquidityOneToken (${titleParams}) ${titleSuffix}`;
+
+			it(wantTitle, async function(this: Mocha.Context) {
+				this.timeout(8 * 1000);
+				this.slow(3.5 * 1000);
+
+				const gotProm: Promise<BigNumber> = TokenSwap.calculateRemoveLiquidityOneToken({
+					chainId:  tc.chainId,
+					lpToken:  tc.lpToken,
+					token:    tc.poolToken,
+					amount:   tc.withdrawAmount
+				});
+
+				let got: BigNumber;
+
+				try {
+					got = await gotProm;
+				} catch (e) {
+					if (tc.wantError) {
+						return (await expect(gotProm).to.eventually.be.rejected)
+					}
+
+					return (await expect(gotProm).to.eventually.not.be.rejected)
+				}
+
+				if (tc.wantZero) {
+					return expect(got).to.eq(Zero)
+				}
+
+				return expect(got).to.be.gt(Zero)
 			});
 		});
 	});
