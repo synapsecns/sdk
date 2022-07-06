c2fbddd18f00f8823361eaa9d47b07589ac7ac6a
metagabbar <metagabbar@gmail.com>
2022-07-05 23:30:03 -0400

    Add tests

--- test/synapsebridge/getEstimatedBridgeOutput-test.ts
+++ test/synapsebridge/getEstimatedBridgeOutput-test.ts
@@ -244,6 +244,10 @@ describe("SynapseBridge - getEstimatedBridgeOutput tests", function(this: Mocha.
         makeTestCase(Tokens.H20,         Tokens.H20,       ChainId.ETH,         ChainId.HARMONY,  randomAmtETH, zeroEstimate, returnsError),
         makeTestCase(Tokens.WBTC,        Tokens.WBTC,      ChainId.ETH,         ChainId.KLAYTN),
         makeTestCase(Tokens.USDC,        Tokens.USDC,      ChainId.KLAYTN,      ChainId.ETH),
+        makeTestCase(Tokens.USDC,        Tokens.USDC,      ChainId.ETH,         ChainId.KLAYTN),
+        makeTestCase(Tokens.WETH,        Tokens.ETH,       ChainId.KLAYTN,      ChainId.ETH),
+        makeTestCase(Tokens.ETH,         Tokens.WETH,      ChainId.ETH,         ChainId.KLAYTN),
+        makeTestCase(Tokens.WETH,        Tokens.WETH,      ChainId.OPTIMISM,    ChainId.KLAYTN),
     ].forEach((tc: TestCase) => {
         const [describeTitle, bridgeOutputTestTitle, transactionTestTitle, approveTestTitle] = makeTestName(tc)
 
