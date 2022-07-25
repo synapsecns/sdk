e969b26ecfd1715e8317292cb83385ec1a6cc680
metagabbar <metagabbar@gmail.com>
2022-07-25 18:14:01 -0400

    Fix bridging amounts from ETH

--- src/bridge/bridge.ts
+++ src/bridge/bridge.ts
@@ -532,7 +532,7 @@ export namespace Bridge {
             });
 
             const checkEthBridge = (c1: number, c2: number, t: Token): boolean =>
-                c1 === ChainId.ETH && (BridgeUtils.isL2ETHChain(c2)) && t.swapType === SwapType.ETH
+                c1 === ChainId.ETH && (BridgeUtils.isL2ETHChain(c2) || c2 === ChainId.DFK) && t.swapType === SwapType.ETH
 
             const
                 isSpecialFrom: boolean = BridgeUtils.isSpecialToken(this.chainId, tokenFrom),
